e42566999097d1ee6e439a55f97abc418b8cfa03
404Paolo <131349768+404Paolo@users.noreply.github.com>
2024-11-29 22:42:09 +0800

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SampleToken is ERC20, Ownable {
+    
+    constructor(address initialOwner) ERC20("SampleToken", "SPT") Ownable(msg.sender) {
+        transferOwnership(initialOwner);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
